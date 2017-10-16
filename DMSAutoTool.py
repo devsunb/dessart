87209df5f402fbb8ed0bbc7dc0c0a8346fd333b2
Lee Jae Seok <infreljs@gmail.com>
2017-10-16 18:23:21 +0900

    Update DMSAutoTool.py

--- DMSAutoTool.py
+++ DMSAutoTool.py
@@ -1,26 +1,16 @@
 import requests
 import time
+import sys
 
 classes = {
-    '다온실': '3',
-    '나온실': '2',
     '가온실': '1',
+    '나온실': '2',
+    '다온실': '3',
     '라온실': '4',
-    '4층 독서실': '6',
     '3층 독서실': '5',
+    '4층 독서실': '6',
     '5층 독서실': '7'
 }
-
-classList = ['다온실', '나온실', '가온실', '라온실', '4층 독서실', '3층 독서실', '5층 독서실']
-seatList = [
-    ['13', '14', '17', '18'],
-    ['1', '3', '7', '11', '15'],
-    ['1', '5', '9', '13', '17'],
-    ['1', '5', '3', '13', '17'],
-    ['1', '9', '17', '25'],
-    ['1', '9', '17', '25'],
-    ['6', '7', '8']
-]
 
 cookie = ''
 
@@ -56,46 +46,43 @@ def login(id, pw):
         return False
 
 
-def reserveSeat(className, seat):
+def reserveSeat(id, class_, seat):
     global cookie
     counter = 0
 
     # Reserve Seat
     while True:
         url = "http://dsm2015.cafe24.com/apply/extension"
-        payload = {'class': classes[className], 'seat': seat}
+        payload = {'class': classes[class_], 'seat': seat}
         headers = {
             'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36', 'cookie': cookie}
         response = requests.put(url, data=payload, headers=headers)
         if response.status_code == 200:
-            print('[SUC] ' + cookie[(cookie.find("vertx-web.session=") + 18):(cookie.find(
-                "vertx-web.session=") + 50)] + ' : ' + className + ' ' + seat + '번 자리 신청 성공!')
+            print('[SUC] ' + id + ' : ' + class_ +
+                  ' ' + seat + '번 자리 신청 성공!')
             return 0
         elif response.status_code == 204:
-            print('[ERR] ' + cookie[(cookie.find("vertx-web.session=") + 18):(cookie.find(
-                "vertx-web.session=") + 50)] + ' : 연장 가능 시간 아님! ' + className + ' ' + seat + '번 자리 신청 실패!')
+            print('[ERR] ' + id + ' : 연장 가능 시간 아님! ' +
+                  class_ + ' ' + seat + '번 자리 신청 실패!')
         elif response.status_code == 400:
-            print('[ERR] ' + cookie[(cookie.find("vertx-web.session=") + 18):(
-                cookie.find("vertx-web.session=") + 50)] + ' : [400] 오류 발생! ' +
-                className + ' ' + seat + '번 자리 신청 실패! 로그인 재시도...')
+            print('[ERR] ' + id + ' : [400] 오류 발생! ' +
+                  class_ + ' ' + seat + '번 자리 신청 실패! 로그인 재시도...')
             login('infreljs', '20412')
         elif response.status_code == 500:
-            print('[ERR] ' + cookie[(cookie.find("vertx-web.session=") + 18):(
-                cookie.find("vertx-web.session=") + 50)] + ' : [500] 오류 발생! ' +
-                className + ' ' + seat + '번 자리는 이미 신청된 자리입니다!!!')
+            print('[ERR] ' + id + ' : [500] 오류 발생! ' +
+                  class_ + ' ' + seat + '번 자리는 이미 신청된 자리입니다!!!')
             return 1
 
         counter += 1
         if counter >= 15:
-            print('[FIN] ' + cookie[(cookie.find("vertx-web.session=") + 18):(cookie.find(
-                "vertx-web.session=") + 50)] + ' : 시도 ' + str(counter) + '회 초과로 종료합니다.')
+            print('[FIN] ' + id + ' : 시도 ' + str(counter) + '회 초과로 종료합니다.')
             return -1
         else:
-            print('[~~~] ' + cookie[(cookie.find("vertx-web.session=") + 18)                                    :(cookie.find("vertx-web.session=") + 50)] + ' : 5초 후 다시 시도...')
+            print('[~~~] ' + id + ' : 5초 후 다시 시도...')
             time.sleep(5)
 
 
-if __name__ == '__main__':
+def autoReserveSeat(id, pw, class_, seat):
     counter = 0
     now = time.localtime()
 
@@ -103,7 +90,7 @@ if __name__ == '__main__':
     print("------ %04d년 %02d월 %02d일 %02d시 %02d분 %02d초 ------" % (now.tm_year, now.tm_mon,
                                                                  now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
     getSession()
-    while login('infreljs', '20412') == False:
+    while login(id, pw) == False:
         counter += 1
         if counter >= 15:
             print('[FIN] 시도 ' + str(counter) + '회 초과로 종료합니다.')
@@ -112,12 +99,27 @@ if __name__ == '__main__':
             print('[ERR] 로그인 실패!!! 5초 후 다시 시도...')
             time.sleep(5)
 
-    for i in range(len(classList)):
-        for j in range(len(seatList[i])):
-            print('[~~~] ' + classList[i] + ' ' +
-                  seatList[i][j] + '번 자리 신청 시도')
-            result = reserveSeat(classList[i], seatList[i][j])
-            if result == 1:
-                continue
-            else:
-                exit()
+    print('[~~~] ' + class_ + ' ' + seat + '번 자리 신청 시도')
+    result = reserveSeat(id, class_, seat)
+    exit()
+
+
+if __name__ == '__main__':
+    try:
+        sel = sys.argv[sys.argv.index('-m') + 1]
+        if(sel == 'seat'):
+            id = sys.argv[sys.argv.index('-u') + 1]
+            pw = sys.argv[sys.argv.index('-p') + 1]
+            class_ = sys.argv[sys.argv.index('-c') + 1]
+            seat = sys.argv[sys.argv.index('-s') + 1]
+
+            autoReserveSeat(id, pw, class_, seat)
+        else:
+            print('Usage : ' + sys.argv[0] +
+                  ' -m (seat|weekend) -u (id) -p (password) -c (class) -s (seat)')
+            exit()
+
+    except (ValueError, IndexError) as e:
+        print('Usage : ' + sys.argv[0] +
+              ' -m (seat|weekend) -u (id) -p (password) -c (class) -s (seat)')
+        exit()
